f66336f7a78f7cc868e1236dc20d6437d6311fa6
Bruno Janeiro <janeiro.bruno23@gmail.com>
2023-08-07 01:21:20 +0200

    Tentativa do Location

--- controllers/products/getProductByLocation.js
+++ controllers/products/getProductByLocation.js
@@ -1,6 +1,4 @@
-const {
-  getProductByLocation,
-} = require('../../db/queries/produtos/getProductByLocation');
+const { getProductByLocation } = require('../../db/queries/produtos/getProductByLocation)
 
 const getProductLocation = async (req, res, next) => {
   try {
