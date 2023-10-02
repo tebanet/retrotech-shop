82bc262802dc24765d4672c7386595f25a53fed9
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2023-10-02 09:24:34 +0200

    merge_head

--- server.js
+++ server.js
@@ -20,8 +20,17 @@ const corsMiddleware = cors({
 
 app.use(corsMiddleware);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://retrotechshop.vercel.app/*'
+  );
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
