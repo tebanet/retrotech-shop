bd8ef94b8b2e10e83f874004217b96f62bfaf30c
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2023-10-02 09:17:12 +0200

    Ajustes cors

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ const corsMiddleware = cors({
 app.use(corsMiddleware);
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://retrotechshop.vercel.app/*'
+  );
+  res.header('Access-Control-Allow-Methods', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
