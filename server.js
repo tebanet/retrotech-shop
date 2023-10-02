70f406aae568bf8da26fbac0b96daac00d733c24
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2023-10-02 10:31:26 +0200

    Cambio en server.js

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(corsMiddleware);*/
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://retrotechshop.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
+
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
