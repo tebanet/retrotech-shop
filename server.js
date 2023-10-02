1606e1b3810a5eda3a3c4e349c5aa9c414376fdc
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2023-10-02 09:22:15 +0200

    ajuste cors

--- server.js
+++ server.js
@@ -20,14 +20,8 @@ const corsMiddleware = cors({
 
 app.use(corsMiddleware);
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
   next();
 });
 
